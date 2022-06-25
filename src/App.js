a6f68f87633a9046b89db85c565606d953c2b70b
Mariam-Madbouli <mariam.madbouli@gmail.com>
2022-06-25 05:25:50 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
+import photo from './images/desktop.jpg'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="card">
+      <div className= "text">
+
+        <div className="textup">
+
+          <h1>Get <span>insights</span> that help your business grow.</h1>
+          <p className="ph">Discover the benetfits of data analytics and make better decisions regarding revenue, customer experience, and overall efficiency.</p>
+
+        
+
+        </div>
+
+        <div className = "textdown">
+          <div className="num">
+            <h3>10K+</h3>
+            <p className = "phar">COMPANIES</p>
+          </div>
+
+          <div className="num">
+            <h3>314</h3>
+            <p className = "phar">TEMPLATES</p>
+          </div>
+
+          <div className="num">
+            <h3>12M+</h3>
+            <p className = "phar">QUERIES</p>
+          </div>
+          
+        </div>
+
+        
+
+      </div>
+      
+      <div className="image">
+        <img src={photo} alt=""/> 
+      </div>
     </div>
   );
 }
